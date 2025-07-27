e119b77ccab28d5e74dc859908d401f8cfc8ed01
halildeu <halildeu@gmail.com>
2025-07-27 12:55:25 +0300

    Depoyu yerel versiyonla tamamen senkronize et

--- apps/web-shell/postcss.config.cjs
+++ apps/web-shell/postcss.config.cjs
@@ -1,11 +1,15 @@
+// postcss.config.cjs
 const path = require('path');
 
 module.exports = {
   plugins: {
     'postcss-nesting': {},
+
+    /* Tailwind v4 PostCSS eklentisi */
     '@tailwindcss/postcss': {
       config: path.resolve(__dirname, '../../tailwind.config.js'),
     },
+
     autoprefixer: {},
   },
 };
